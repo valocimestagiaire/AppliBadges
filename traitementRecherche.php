da6485b91e2c1f1a68db40881df87ecb2aeab684
valocimestagiaire <stagiaire@valocime.fr>
2021-06-18 16:06:35 +0200

    Meilleure recherche

--- traitementRecherche.php
+++ traitementRecherche.php
@@ -28,41 +28,72 @@
 				$connexion = connexion();
 				$id = $_POST['srch-term'];
 				
-				$queryIdTele = mysqli_query($connexion,"SELECT Nom,Prénom,Id_Télécommande,Statut FROM telecommande NATURAL JOIN identites WHERE Id_Télécommande='$id'");
+				$queryIdTele = mysqli_query($connexion,"SELECT * FROM telecommande LEFT JOIN identites ON telecommande.Id_Identité=identites.Id_Identité WHERE Id_Télécommande LIKE '%$id%'");
 				if($nb_lignes = mysqli_num_rows($queryIdTele) > 0){
-					$tele = mysqli_fetch_array($queryIdTele);
-					echo"<td>".$tele['Nom']."</td><td>".$tele['Prénom']."</td><td>Télécommande</td><td>".$tele['Id_Télécommande']."</td><td>".$tele['Statut']."</td>";
+					foreach($queryIdTele as $tele){
+						if(!isset($tele['Nom'])){
+							$tele['Nom'] = "Non Attribué";
+							$tele['Prénom'] = "Non Attribué";
+						}
+						echo"<tr><td>".$tele['Nom']."</td><td>".$tele['Prénom']."</td><td>Télécommande</td><td>".$tele['Id_Télécommande']."</td><td>".$tele['Statut']."</td></tr>";
+					}
 				}	
 				
-				$queryIdBadgeN = mysqli_query($connexion,"SELECT Nom,Prénom,Id_Badge_Noir,Statut FROM badge_noir NATURAL JOIN identites WHERE Id_Badge_Noir='$id'");
+				$queryIdBadgeN = mysqli_query($connexion,"SELECT * FROM badge_noir LEFT JOIN identites ON badge_noir.Id_Identité=identites.Id_Identité WHERE Id_Badge_Noir LIKE '%$id%'");
 				if($nb_lignes = mysqli_num_rows($queryIdBadgeN) > 0){
-					$badgeN = mysqli_fetch_array($queryIdBadgeN);
-					echo"<td>".$badgeN['Nom']."</td><td>".$badgeN['Prénom']."</td><td>Badge Noir</td><td>".$badgeN['Id_Badge_Noir']."</td><td>".$badgeN['Statut']."</td>";
+					foreach($queryIdBadgeN as $badgeN){
+						if(!isset($badgeN['Nom'])){
+							$badgeN['Nom'] = "Non Attribué";
+							$badgeN['Prénom'] = "Non Attribué";
+						}
+						echo"<tr><td>".$badgeN['Nom']."</td><td>".$badgeN['Prénom']."</td><td>Badge Noir</td><td>".$badgeN['Id_Badge_Noir']."</td><td>".$badgeN['Statut']."</td></tr>";
+					}
 				}
 				
-				$queryIdBadgeB = mysqli_query($connexion,"SELECT Nom,Prénom,Id_Badge_Bleu,Statut FROM badge_bleu NATURAL JOIN identites WHERE Id_Badge_Bleu='$id'");
+				$queryIdBadgeB = mysqli_query($connexion,"SELECT * FROM badge_bleu LEFT JOIN identites ON badge_bleu.Id_Identité=identites.Id_Identité WHERE Id_Badge_Bleu LIKE '%$id%'");
 				if($nb_lignes = mysqli_num_rows($queryIdBadgeB) > 0){
-					$badgeB = mysqli_fetch_array($queryIdBadgeB);
-					echo"<td>".$badgeB['Nom']."</td><td>".$badgeB['Prénom']."</td><td>Badge Bleu</td><td>".$badgeB['Id_Badge_Bleu']."</td><td>".$badgeB['Statut']."</td>";
+					foreach($queryIdBadgeB as $badgeB){
+						if(!isset($badgeB['Nom'])){
+							$badgeB['Nom'] = "Non Attribué";
+							$badgeB['Prénom'] = "Non Attribué";
+						}
+						echo"<tr><td>".$badgeB['Nom']."</td><td>".$badgeB['Prénom']."</td><td>Badge Bleu</td><td>".$badgeB['Id_Badge_Bleu']."</td><td>".$badgeB['Statut']."</td></tr>";
+					}
 				}
 				
-				$queryIdBadgeC = mysqli_query($connexion,"SELECT Nom,Prénom,Id_Café,Statut FROM cafe NATURAL JOIN identites WHERE Id_Café='$id'");
+				$queryIdBadgeC = mysqli_query($connexion,"SELECT * FROM cafe LEFT JOIN identites ON cafe.Id_Identité=identites.Id_Identité WHERE Id_Café LIKE '%$id%'");
 				if($nb_lignes = mysqli_num_rows($queryIdBadgeC) > 0){
-					$badgeC = mysqli_fetch_array($queryIdBadgeC);
-					echo"<td>".$badgeC['Nom']."</td><td>".$badgeC['Prénom']."</td><td>Badge Café</td><td>".$badgeC['Id_Café']."</td><td>".$badgeC['Statut']."</td>";
+					foreach($queryIdBadgeC as $badgeC){
+						if(!isset($badgeC['Nom'])){
+							$badgeC['Nom'] = "Non Attribué";
+							$badgeC['Prénom'] = "Non Attribué";
+						}
+						echo"<tr><td>".$badgeC['Nom']."</td><td>".$badgeC['Prénom']."</td><td>Badge Café</td><td>".$badgeC['Id_Café']."</td><td>".$badgeC['Statut']."</td></tr>";
+					}
 				}
 				
-				$queryIdBadgeP = mysqli_query($connexion,"SELECT Nom,Prénom,Id_Parking,Statut FROM parking NATURAL JOIN identites WHERE Id_Parking='$id'");
+				$queryIdBadgeP = mysqli_query($connexion,"SELECT * FROM parking LEFT JOIN identites ON parking.Id_Identité=identites.Id_Identité WHERE Id_Parking LIKE '%$id%'");
 				if($nb_lignes = mysqli_num_rows($queryIdBadgeP) > 0){
-					$badgeP = mysqli_fetch_array($queryIdBadgeP);
-					echo"<td>".$badgeP['Nom']."</td><td>".$badgeP['Prénom']."</td><td>Badge Parking</td><td>".$badgeP['Id_Parking']."</td><td>".$badgeP['Statut']."</td>";
+					foreach($queryIdBadgeP as $badgeP){
+						if(!isset($badgeP['Nom'])){
+							$badgeP['Nom'] = "Non Attribué";
+							$badgeP['Prénom'] = "Non Attribué";
+						}
+						echo"<tr><td>".$badgeP['Nom']."</td><td>".$badgeP['Prénom']."</td><td>Badge Parking</td><td>".$badgeP['Id_Parking']."</td><td>".$badgeP['Statut']."</td></tr>";
+					}
 				}
 				
-				$queryIdBadgeA = mysqli_query($connexion,"SELECT Nom,Prénom,Id_Alarme,Statut FROM alarme NATURAL JOIN identites WHERE Id_Alarme='$id'");
+				$queryIdBadgeA = mysqli_query($connexion,"SELECT * FROM alarme LEFT JOIN identites ON alarme.Id_Identité=identites.Id_Identité WHERE Id_Alarme LIKE '%$id%'");
 				if($nb_lignes = mysqli_num_rows($queryIdBadgeA) > 0){
-					$badgeA = mysqli_fetch_array($queryIdBadgeA);
-					echo"<td>".$badgeA['Nom']."</td><td>".$badgeA['Prénom']."</td><td>Alarme</td><td>".$badgeA['Id_Alarme']."</td><td>".$badgeA['Statut']."</td>";
+					foreach($queryIdBadgeA as $badgeA){
+						if(!isset($badgeA['Nom'])){
+							$badgeA['Nom'] = "Non Attribué";
+							$badgeA['Prénom'] = "Non Attribué";
+						}
+						echo"<tr><td>".$badgeA['Nom']."</td><td>".$badgeA['Prénom']."</td><td>Alarme</td><td>".$badgeA['Id_Alarme']."</td><td>".$badgeA['Statut']."</td></tr>";
+					}
 				}
+
 				
 				
 				mysqli_close($connexion);
